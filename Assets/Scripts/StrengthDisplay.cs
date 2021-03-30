d358dd117dbd2533eaf97dcf8ce0b42685294dcd
Charlee <liucharlee@gmail.com>
2021-03-29 17:12:15 -0700

    Commit

---
+++ Assets/Scripts/StrengthDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StrengthDisplay : MonoBehaviour
+{
+    public int strength;
+    public Text strengthText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        strengthText.text = "STRENGTH : " + strength;
+
+        /*if(Input.GetKeyDown(KeyCode.Space)){
+            strength = Random.Range(1, 10);
+        }*/
+    }
+
+    public void Generate(){
+        strength = Random.Range(1, 10);
+    }
+}
---
+++ Assets/Scripts/ToughnessDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToughnessDisplay : MonoBehaviour
+{
+    public int points = 5;
+    public Text pointsText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = "TOUGHNESS : " + points;
+
+        /*if(Input.GetKeyDown(KeyCode.Space)){
+            strength = Random.Range(1, 10);
+        }*/
+    }
+
+    public void Generate(){
+        points = Random.Range(1, 10);
+    }
+}
