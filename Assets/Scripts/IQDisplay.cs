b1bf23bdc52c31176160aece749e2b9381e37018
Charlee <liucharlee@gmail.com>
2021-04-02 22:54:59 -0700

    SkillDisplays

---
+++ Assets/Scripts/IQDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IQDisplay : MonoBehaviour
+{
+    public int points = 5;
+    public Text pointsText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = "IQ : " + points;
+
+        /*if(Input.GetKeyDown(KeyCode.Space)){
+            strength = Random.Range(1, 10);
+        }*/
+    }
+
+    public void Generate(){
+        points = Random.Range(1, 10);
+    }
+}
